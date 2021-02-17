f3f2dfd8a17d4be31aa124e3d3e0f53b4c3928a6
Denilson Granados <dbg307@nyu.edu>
2021-02-17 11:37:14 -0500

    Day 1 work

--- Q5_gui.py
+++ Q5_gui.py
@@ -1,14 +1,18 @@
 import math
 import tkinter as tk    
+def call(label,n):
+    s=n.get()
+    label.config(text='Number of inflows is '+s)
+    return 
 root= tk.Tk()
-greeting=tk.Label(
-        text='Name')
-        #bg= 'black', #bg= background
-        #fg= 'white') #fg= forground
-entry=tk.Entry(root)
-greeting.pack()
-entry.pack()
-name=entry.get()
-print(name)
+root.title("Mass Balance")
+root.geometry('400x250')# This size can be changed if needed
+n_inflows_txt= tk.Label(root,text= 'How many inflows?')
+n_inflows_txt.grid(row=1)
+n_inflows_ans= tk.Entry(root)
+n_inflows_ans.grid(row=2)
+Num_inflows= tk.Label(root)
+Num_inflows.grid(row=7)
+n_flow_input=tk.Button(root, text="Next", relief='raised',command=lambda:call(Num_inflows, n_inflows_ans))
+n_flow_input.grid(row=3)
 root.mainloop()
-print(name)
